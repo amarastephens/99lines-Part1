f873fcf112e1c8af88655543abff8b0304e3c926
Amara Stephens <amarastephens@Amaras-MBP.attlocal.net>
2021-09-08 17:03:02 -0500

    first submission

---
+++ app.js
@@ -0,0 +1,7 @@
+let friends = ["Ryan", "Autumn", "Avery", "Ella", "Meghan"];  
+for (let i = 99; i>=1; i--) {
+    console.log(99 + " " + "lines of code in the file")
+} 
+for (let i=99; i>=1; i--) {
+    console. log (99 + " " + "lines of code")
+}
