f25deb5bacda6850e795fd3b79bf1db0d5ee64fb
Amara Stephens <amarastephens@Amaras-MBP.attlocal.net>
2021-09-08 22:25:50 -0500

    finished submission

--- app.js
+++ app.js
@@ -1,7 +1,66 @@
-let friends = ["Ryan", "Autumn", "Avery", "Ella", "Meghan"];  
-for (let i = 99; i>=1; i--) {
-    console.log(99 + " " + "lines of code in the file")
-} 
-for (let i=99; i>=1; i--) {
-    console. log (99 + " " + "lines of code")
+
+let friends = ["Ryan", "Autumn", "Avery", "Ella", "Meghan"];
+ for (j= 0; j <= 4; j++) {
+
+let lines;
+for (i = 99; i >= 1; i--) 
+{
+    if (i == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(i+" "+lines+" of code in the file");
+    if (i < 99) {
+        console.log(i+" "+lines+" of code in the file,");
+    }
+    console.log(i+" "+lines+" of code.");
+
+    console.log(friends[j]+ " strikes one out,")
+
+    console.log("Clears it all out,");
+    if (i == 1) {
+        console.log("No more lines of code in the file!");
+    }
 }
+ }
+    
+    
+    // let friends = ["Ryan", "Autumn", "Avery", "Ella", "Meghan"];
+    // function beerSong() {  
+    //     let lines;
+    //     let linesLeft;
+    //     for (i = 99; i >= 1; i--) {
+    //       if (i == 1) {
+    //         let lines = "line";
+    //         let linesLeft = "No more lines of code in the file.";
+    //       } else {
+    //         let lines = "lines";
+    //         let bottlesLeft = i - 1 + " lines of code in the file!";
+    //       } console.log(i+ " " + lines + " of code in the file");
+    //       console.log(i+ " " + lines + " of code,");
+    //       console.log("Strikes one out, clears it all out,");
+    //       console.log(linesLeft);
+    //       } 
+          
+    //   }
+    //   console.log(beerSong());
+
+
+
+
+
+// for (let i = 99; i >= 1; i--) {
+//     console.log(99 + " " + "lines of code in the file")
+// }
+// if (i> 1) {
+//     console.log("lines of code in the file")
+// }
+// else {
+//     ("line of code in the file")
+// }
+
+// for (let i = 99; i >= 1; i--) {
+//     console.log(99 + " " + "lines of code")
+// }
+
